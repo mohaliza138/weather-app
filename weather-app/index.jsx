247a64dad2392babe46f7f7a5223a62e580ae260
Mohammad Alizadeh <mohaliza138@gmail.com>
2024-06-14 13:47:40 +0330

    Showing data

--- weather-app/index.jsx
+++ weather-app/index.jsx
@@ -10,21 +10,61 @@ function Search({search, setSearch, startSearch}) {
 
 export default function WeatherApp() {
   const [search, setSearch] = useState('');
-
+  const [loading, setLoading] = useState(false);
+  const [wData, setWData] = useState(null);
 
   async function getWeatherData(search) {
+
+    setLoading = true;
+
     try {
       const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=82a5cb665215c764d3a858b79f012ff9`);
 
       const data = await res.json();
+      if(data) {
+        setLoading = false;
+        setWData = data;
+      }
     } catch (e) {
+      setLoading = false;
       console.log(e);
     }
   }
 
-  async function startSearch() {}
+  async function startSearch() {
+    getWeatherData(search);
+  }
 
   return <div>
     <Search search={search} setSearch={setSearch} startSearch={startSearch}/>
+    {loading ? (
+        <div className="loading">Loading...</div>
+      ) : (
+        <div>
+          <div className="city">
+            <h2>
+              {weatherData?.name}
+            </h2>
+          </div>
+          <div className="temp">{weatherData?.main?.temp}</div>
+          <p className="description">
+            {weatherData && weatherData.weather && weatherData.weather[0] ? weatherData.weather[0].description : ""}
+          </p>
+          <div className="info">
+            <div className="col">
+              <div>
+                <p>Wind Speed</p>
+                <p className="wind-speed">{weatherData?.wind?.speed}</p>
+              </div>
+            </div>
+            <div className="col">
+              <div>
+                <p>Humidity</p>
+                <p className="humidity">{weatherData?.main?.humidity}%</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
   </div>;
 }
