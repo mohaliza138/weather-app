fe4bd44fdcf308e5d82fefd11d74fe8c9ca110c8
Mohammad Alizadeh <mohaliza138@gmail.com>
2024-06-14 12:58:04 +0330

    Connect to OpenWeather API
    Implemented function getWeatherData which uses OpenWeather API to get wether data.

--- weather-app/index.jsx
+++ weather-app/index.jsx
@@ -1,17 +1,30 @@
 import { useEffect, useState } from "react";
 import "../styles.css";
 
-function Search({search, setSearch}) {
+function Search({search, setSearch, startSearch}) {
   return <div className="search-engine">
     <input className="search-input" type="text" placeholder="City name" value={search} onChagne={(event)=>setSearch(event.target.value)}/>
-    <button className="search-button">Search</button>
+    <button className="search-button" onClick={startSearch}>Search</button>
   </div>;
 }
 
 export default function WeatherApp() {
   const [search, setSearch] = useState('');
 
+
+  async function getWeatherData(search) {
+    try {
+      const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=82a5cb665215c764d3a858b79f012ff9`);
+
+      const data = await res.json();
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  async function startSearch() {}
+
   return <div>
-    <Search search={search} setSearch={setSearch}/>
+    <Search search={search} setSearch={setSearch} startSearch={startSearch}/>
   </div>;
 }
